4e5ab537877f266fe27b974442dba0bb920c8cb9
Albert Leng Chan Seng <albertleng@yahoo.com>
2023-08-23 14:58:14 +0800

    Complete parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -22,15 +22,37 @@ The parity of a binary word is 1 if the numbe of 1s in the word is odd; otherwis
 #         x &= x - 1 # Reset the 1st set bit
 #     return result
 
-def parity(x: int) -> int:
-    x ^= x >> 32
-    x ^= x >> 16
-    x ^= x >> 8
-    x ^= x >> 4
-    x ^= x >> 2
-    x ^= x >> 1
+# 3.
+# def parity(x: int) -> int:
+#     x ^= x >> 32
+#     x ^= x >> 16
+#     x ^= x >> 8
+#     x ^= x >> 4
+#     x ^= x >> 2
+#     x ^= x >> 1
+#
+#     return x & 1
 
-    return x & 1
+def compute_parity(x: int) -> int:
+    result = 0
+    while x:
+        result ^= 1
+        x &= x - 1
+    return result
+
+
+MASK_SIZE = 16
+BIT_MASK = 0xFFFF
+
+
+def parity(x: int) -> int:
+    # Create a lookup table for 16-bit numbers
+    parity_lookup = [compute_parity(i) for i in range(65536)]
+
+    return (parity_lookup[x & BIT_MASK] ^
+            parity_lookup[(x >> MASK_SIZE) & BIT_MASK] ^
+            parity_lookup[(x >> (2 * MASK_SIZE)) & BIT_MASK] ^
+            parity_lookup[(x >> (3 * MASK_SIZE)) & BIT_MASK])
 
 
 if __name__ == '__main__':
