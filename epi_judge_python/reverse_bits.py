566c104d2070b34475f2d018ad1109b78e7efee7
Albert Leng Chan Seng <albertleng@yahoo.com>
2023-08-25 15:37:39 +0800

    Complete reverse_bits.py

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,10 +1,41 @@
 from test_framework import generic_test
 
 
-def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+# Solution 1: Using pre-calculated cache
+def compute_reverse_bits(i: int) -> int:
+    start = 0
+    end = 15  # last index of a 16-bit integer
 
+    while start < end:
+        if (i >> start) & 1 != (i >> end) & 1:
+            bit_mask = (1 << start) | (1 << end)
+            i ^= bit_mask
+        start += 1
+        end -= 1
+    return i
+
+
+def reverse_bits(x: int) -> int:
+    precomputed_reverse_bits = [compute_reverse_bits(i) for i in range(65535)]
+    mask_size = 16
+    bit_mask = 0xFFFF
+    return precomputed_reverse_bits[x & bit_mask] << (3 * mask_size) | precomputed_reverse_bits[
+        (x >> mask_size) & bit_mask] << (2 * mask_size) | precomputed_reverse_bits[
+        (x >> (2 * mask_size)) & bit_mask] << mask_size | precomputed_reverse_bits[(x >> (3 * mask_size)) & bit_mask]
+
+
+# Solution 2: Without cache. It seems to be faster
+# def reverse_bits(i: int) -> int:
+#     start = 0
+#     end = 63  # last index of a 64-bit integer
+#
+#     while start < end:
+#         if (i >> start) & 1 != (i >> end) & 1:
+#             bit_mask = (1 << start) | (1 << end)
+#             i ^= bit_mask
+#         start += 1
+#         end -= 1
+#     return i
 
 if __name__ == '__main__':
     exit(
